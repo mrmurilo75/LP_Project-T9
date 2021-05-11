719e1c44e943f50d1945044d74ddfa92fd95d55e
mrmurilo75 <mrmurilo75@gmail.com>
2021-05-11 20:23:38 +0100

    @src : create and begin implementing string.c

---
+++ src/string.c
@@ -0,0 +1,11 @@
+String new_String(int size, int* value);
+	// create a new String object
+
+int String_compare(String a, String b);
+	// compare String objects
+	// return -1 if a < b
+	// 	0 if equal
+	// 	1 if a > b
+
+int String_equals(String a, String b);
+	// return non-zero int if the value of the Strings *are* the same
