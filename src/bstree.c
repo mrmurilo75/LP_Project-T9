2040e60c108b22d7535e31cde9121f2bbe30bb82
TiagojmG <80461450+TiagojmG@users.noreply.github.com>
2021-05-12 20:43:59 +0100

    Atualizacao do bstree

--- src/bstree.c
+++ src/bstree.c
@@ -23,3 +23,57 @@ void* BSTMNode_getValue(BSTMNode node) {
 void BSTMNode_setValue(BSTMNode node, void* value) {
     node->value = value;
 }
+
+void* BSTreeMap_find(BSTreeMap tree, void* key);
+/*
+    link STsearch(link h, int v) {
+        int t;
+        if (h == NULL) return NULL;
+            t = h->chave;
+        if (v == t) return h;
+        if (v < t)
+            return STsearch(h->l, v);
+        else
+            return STsearch(h->r, v);
+    }
+ */
+
+void BSTreeMap_insert(BSTreeMap tree, void* key, void* value);
+/*
+    link STinsertR(link h, int v)
+    {
+        if (h == NULL) {
+            link x = malloc(sizeof *x);
+            x->chave = v;
+            x->l = x->r = NULL;
+            return x;
+        }
+        if (v < h->chave) {
+            h->l = STinsertR(h->l, v);
+            h = rotR(h);
+        }
+        else {
+            h->r = STinsertR(h->r, v);
+            h = rotL(h);
+        }
+        return h;
+    }
+
+    link rotR(link h)
+    {
+        link x = h->l;
+        h->l = x->r;
+        x->r = h;
+        return x;
+    }
+
+    link rotL(link h)
+    {
+        link x = h->r;
+        h->r = x->l;
+        x->l = h;
+        return x;
+    }
+ */
+
+void BSTreeMap_remove(BSTreeMap tree, void* key);
