819beb490fe13bd9ef51fbd7f84948ff3df895ef
Murilo Rosa <mrmurilo75@gmail.com>
2021-05-20 13:09:51 +0100

    @src : begin Main with GUI

---
+++ src/Main.c
@@ -0,0 +1,27 @@
+#include <gtk/gtk.h>
+
+GtkWidget *window,
+	*vbox,
+	*fullTxt,
+	*curWord,
+	*btn0,
+	*btn1,
+	*btn2,
+	*btn3,
+	*btn4,
+	*btn5,
+	*btn6,
+	*btn7,
+	*btn8,
+	*btn9,
+	*btnCycle,
+	*btnSend;
+
+
+int main(int argc, char* argv[]) {
+
+	gtk_init(&argc, & argv);
+
+
+
+}
