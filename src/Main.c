7f6b73e5012a09911ab8deac290330bf41534ec7
Murilo Rosa <mrmurilo75@gmail.com>
2021-05-22 18:04:43 +0100

    @src/Main : create buttons layout

--- src/Main.c
+++ src/Main.c
@@ -1,10 +1,11 @@
 #include <gtk/gtk.h>
 
 GtkWidget *window,
-	*vbox,
+	*grid,
 	*fullTxt,
 	*curWord,
-	*btn0,
+	*buttons[12];
+	/*
 	*btn1,
 	*btn2,
 	*btn3,
@@ -15,13 +16,47 @@ GtkWidget *window,
 	*btn8,
 	*btn9,
 	*btnCycle,
+	*btn0,
 	*btnSend;
+	*/
+
+gchar *buttonLabels[12] = { 
+	"1", "2", "3", 
+	"4", "5", "6", 
+	"7", "8", "9", 
+	"C", "0", "S" 
+	}; 
 
 
 int main(int argc, char* argv[]) {
 
 	gtk_init(&argc, & argv);
 
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Project T9");
+	gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+	gtk_container_set_border_width(GTK_CONTAINER(window), 5);
 
+	grid = gtk_grid_new();
+	gtk_grid_set_row_spacing(GTK_GRID(grid), 2);
+	gtk_grid_set_column_spacing(GTK_GRID(grid), 2);
 
+	for(int line = 0, pos = 0; line < 4; line++)
+		for(int col = 0; col < 3; col++) {
+			buttons[pos] = gtk_button_new_with_label(buttonLabels[pos]);
+			gtk_grid_attach(GTK_GRID(grid), buttons[pos], col, line, 1, 1);
+			pos++;
+		}
+
+	gtk_container_add(GTK_CONTAINER(window), grid);
+
+	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+	gtk_widget_show_all(window);
+
+	gtk_main();
+
+	return 0;
 }
+
