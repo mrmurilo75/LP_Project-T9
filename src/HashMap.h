0af3f6d52dda0b95e8b883410b46ab79710523f7
mrmurilo75 <mrmurilo75@gmail.com>
2021-05-27 01:04:31 +0100

    @src/HashMap : fix errors

--- src/HashMap.h
+++ src/HashMap.h
@@ -9,19 +9,16 @@ HashMap new_HashMap() {
 }
 
 void* HashMap_get(HashMap map, void* key) {
-	BSTreeMap lmap[100000] = (*BSTreeMap) map;
-	int lkey = *( (*int) key );
+	BSTreeMap *lmap = (BSTreeMap*) map;
+	int lkey = *( (int*) key );
 	
-	if( (int) lmap[lkey] == 0 )
-		return NULL;
-
 	return lmap[lkey];
 }
 
 
 void HashMap_insert(HashMap map, void* key, void* value) {
-	BSTreeMap lmap[100000] = (*BSTreeMap) map;
-	int lkey = *( (*int) key );
+	BSTreeMap *lmap = (BSTreeMap*) map;
+	int lkey = *( (int*) key );
 
 	lmap[lkey] = (BSTreeMap) value;
 
